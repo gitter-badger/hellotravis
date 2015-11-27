f4e2569309c1143a700e30dddc4593a852db159d
廖舜茂 <ooxxmix@liaoshunmaodeiMac.local>
2015-11-27 18:04:34 +0800

    2

--- app/src/main/java/ursapictor/hellotravis/MainActivity.java
+++ app/src/main/java/ursapictor/hellotravis/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        test1
+//        test2
         setContentView(R.layout.activity_main);
     }
 }
